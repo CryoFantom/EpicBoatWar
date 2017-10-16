7e1f9783c6d9a2c0a3f74ef56f506794fa497bec
Yuukih <32818181+Yuukih@users.noreply.github.com>
2017-10-16 18:42:40 +0200

    Add files via upload

--- DemoAffBat.pas
+++ DemoAffBat.pas
@@ -7,19 +7,32 @@ var joueur1, joueur2: Joueur;
 
 
 begin
-	joueur1.nbBateaux:=1;
-	joueur2.nbBateaux:=1;
-	game.joueur1joue:= true;
+	joueur1.nbBateaux:=2;
+	joueur2.nbBateaux:=2;
+	game.joueur1joue:= false;
 	joueur2.boat[1].pos[1].x:=2;
 	joueur2.boat[1].pos[1].y:=2;
 	joueur2.boat[1].pos[2].x:=3;
 	joueur2.boat[1].pos[2].y:=2;
-	joueur1.boat[1].pos[1].x:=4;
-	joueur1.boat[1].pos[1].y:=4;
-	joueur1.boat[1].pos[2].x:=3;
-	joueur1.boat[1].pos[2].y:=4;
+	joueur1.boat[1].pos[1].x:=1;
+	joueur1.boat[1].pos[1].y:=1;
+	joueur1.boat[1].pos[2].x:=1;
+	joueur1.boat[1].pos[2].y:=2;
+	joueur2.boat[2].pos[1].x:=6;
+	joueur2.boat[2].pos[1].y:=6;
+	joueur2.boat[2].pos[2].x:=6;
+	joueur2.boat[2].pos[2].y:=7;
+	joueur1.boat[2].pos[1].x:=5;
+	joueur1.boat[2].pos[1].y:=1;
+	joueur1.boat[2].pos[2].x:=5;
+	joueur1.boat[2].pos[2].y:=2;
 	joueur1.boat[1].taille:=2;
 	joueur2.boat[1].taille:=2;
+	joueur2.boat[2].taille:=2;
+	joueur1.boat[2].taille:=2;
 	joueur2.boat[1].detecte:=true;
+	joueur2.boat[2].detecte:=true;
+	joueur1.boat[1].detecte:=true;
+	joueur1.boat[2].detecte:=true;
 	affBateaux(game, joueur1, joueur2);
 end.
